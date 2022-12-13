04ae4d4e46a49780fde47a078943415f97574694
Emilio Mendoza <52044603+Ironislife98@users.noreply.github.com>
2022-12-13 08:59:23 -0500

    Added Basic Player Movement

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    public float moveSpeed = 5f; // movement speed of the player
+
+    private Vector2 movement; // variable to store the player's movement input
+
+    private Rigidbody2D rb; // reference to the player's Rigidbody2D component
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // get the player's Rigidbody2D component
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // get the player's horizontal and vertical input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // FixedUpdate is called at a fixed interval (used for physics updates)
+    void FixedUpdate()
+    {
+        // move the player by applying a force to the player's Rigidbody2D
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
